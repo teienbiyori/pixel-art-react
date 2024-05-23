fca4b08a3eee5e9443aad66b7d658646f32c337d
Sandra Sika <shika4days@gmail.com>
2024-05-24 02:19:08 +0800

    feat: adjust Task component in tracker

--- src/components/tracker.jsx
+++ src/components/tracker.jsx
@@ -1,21 +1,46 @@
+import { useState } from "react"
+
+function TypingArea(){
+  return(<>
+    <div className="typing-area">
+      <div className="selector-wrapper">
+        <select name="category">
+          <option value="feat">Feat</option>
+          <option value="fix">Fix</option>
+          <option value="refactor">Refactor</option>
+          <option value="other">Other</option>
+        </select>
+      </div>
+      <input type="text" className="task-input" />
+      <button className="submit"><i className="fa-solid fa-square-plus"></i></button>
+    </div>
+  </>)
+}
+
 function Category(){
   return(<>
     <div className="tag-wrapper">
-      <button className="category-selector">feat</button>
-      <button className="category-selector">fix</button>
-      <button className="category-selector">refactor</button>
-      <button className="category-selector">all</button>
+      <button className="category-selector">Feat</button>
+      <button className="category-selector">Fix</button>
+      <button className="category-selector">Refactor</button>
+      <button className="category-selector">Other</button>
+      <button className="category-selector"><i className="fa-solid fa-rotate-left"></i></button>
     </div>
   </>)
 }
 
 function Task(){
+  const [done, setDone] = useState(false);
+  const handleDone = () =>{
+    setDone(!done);
+  }
   return(<>
     <div className="each-task">
-      <span className="category">feat</span>
+      <div className="category">refactor</div>
       <input type="text"/>
-      <button className="task-complete">click</button>
-      <span className="drag-icon">...</span>
+      <button className="task-complete" onClick={handleDone}><i className={done? "fa-solid fa-square-check":"fa-regular fa-square-check"}></i></button>
+      <button className="task-delete"><i className="fa-solid fa-trash-can"></i></button>
+      <span className="drag-icon"><i className="fa-solid fa-ellipsis-vertical"></i></span>
     </div>
   </>)
 }
@@ -25,12 +50,8 @@ function Task(){
 export default function Tracker(){
   return(<>
   <div className="list-wrapper">
-    <div className="typing-area">
-      {/* drop-list */}
-      <input type="text" className="task-input" />
-      <button className="submit">drop icon</button>
-    </div>
-     <Category/>
+    <TypingArea/>
+    <Category/>
     <div className="task-area">
       <Task/>
       <Task/>
