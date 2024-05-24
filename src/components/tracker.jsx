6ce742f06d2902d4f94b9a0c91950060fe7fd024
Sandra Sika <shika4days@gmail.com>
2024-05-25 03:02:33 +0800

    feat: add func to typing area btn

--- src/components/tracker.jsx
+++ src/components/tracker.jsx
@@ -1,18 +1,35 @@
 import { useState } from "react"
+import PropTypes from "prop-types"
+
+
+Task.propTypes = {
+  category: PropTypes.string,
+  title: PropTypes.string,
+}
 
 function TypingArea(){
+  const [category, setCategory] = useState("feat")
+  const [title, setTitle] = useState("")
+  const handleCategory = (e) =>{
+    setCategory(e.target.value);
+  }
+  const handleAdd = () => {
+    //post title & category
+    console.log(category + " , " + title)
+
+  }
   return(<>
     <div className="typing-area">
       <div className="selector-wrapper">
-        <select name="category">
+        <select name="category" onChange={handleCategory}>
           <option value="feat">Feat</option>
           <option value="fix">Fix</option>
           <option value="refactor">Refactor</option>
           <option value="other">Other</option>
         </select>
       </div>
-      <input type="text" className="task-input" />
-      <button className="submit"><i className="fa-solid fa-square-plus"></i></button>
+      <input type="text" className="task-input" placeholder="Enter task" onChange={(e)=>{setTitle(e.target.value)}} value={title}/>
+      <button className="submit" onClick={handleAdd}><i className="fa-solid fa-square-plus"></i></button>
     </div>
   </>)
 }
@@ -29,15 +46,15 @@ function Category(){
   </>)
 }
 
-function Task(){
+function Task({category, title}){
   const [done, setDone] = useState(false);
   const handleDone = () =>{
     setDone(!done);
   }
   return(<>
     <div className="each-task">
-      <div className="category">refactor</div>
-      <input type="text"/>
+      <div className="category">{category}</div>
+      <input type="text" value={title}/>
       <button className="task-complete" onClick={handleDone}><i className={done? "fa-solid fa-square-check":"fa-regular fa-square-check"}></i></button>
       <button className="task-delete"><i className="fa-solid fa-trash-can"></i></button>
       <span className="drag-icon"><i className="fa-solid fa-ellipsis-vertical"></i></span>
@@ -53,10 +70,6 @@ export default function Tracker(){
     <TypingArea/>
     <Category/>
     <div className="task-area">
-      <Task/>
-      <Task/>
-      <Task/>
-      <Task/>
       <Task/>
     </div>
   </div>
